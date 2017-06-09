b0d524f69076fcc987ed9071c565ffee22e8c2fe
shaolin PC2 <abbenyyyyyy@qq.com>
2017-06-09 15:37:03 +0800

    complete fetchDataFromInternet and V1.0

--- src/EuramericanMoviesScreen.js
+++ src/EuramericanMoviesScreen.js
@@ -1,10 +1,69 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+import React, { PureComponent } from 'react';
+import { StyleSheet, TouchableOpacity, Text, Image, FlatList, Dimensions } from 'react-native';
 
-export default class EuramericanMoviesScreen extends Component{
-    render(){
-        return(
-            <Text>EuramericanMoviesScreen</Text>
+export default class EuramericanMoviesScreen extends PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            listData: null
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://abben-version.oss-cn-shenzhen.aliyuncs.com/EuramericanMovies.json")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    listData: responseJson
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    _keyExtractor = (item, index) => item.name;
+
+    _renderIten = ({ item }) => {
+        return (
+            <TouchableOpacity style={styles.viewStyle} activeOpacity={0.8} onPress={() => { this.props.navigation.navigate('MovieDetailsScreen', { item: item }) }}>
+                <Image source={{ uri: item.imageOfMovie }} style={styles.imgStyle} />
+                <Text style={styles.textStyle}>{item.name}</Text>
+            </TouchableOpacity>
+        )
+    };
+
+    render() {
+        return (
+            <FlatList
+                data={this.state.listData}
+                numColumns={2}
+                keyExtractor={this._keyExtractor}
+                renderItem={this._renderIten}
+            />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderColor: '#efefef',
+        borderStyle: ('solid'),
+        borderWidth: 1
+    },
+    imgStyle: {
+        // 设置宽度
+        width: Dimensions.get('window').width / 2 - 10,
+        // 设置高度
+        height: 280,
+        marginLeft: 5,
+        marginRight: 5,
+        // 设置图片填充模式
+        resizeMode: 'contain'
+    },
+    textStyle: {
+        color: '#212121',
+        textAlign: 'center'
+    }
+});
--- src/MovieDetailsScreen.js
+++ src/MovieDetailsScreen.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { Button, Image, Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Button, Image, Text, TouchableHighlight, ScrollView, View, StyleSheet, Dimensions, Platform, PixelRatio, Linking } from 'react-native';
 
 export default class MovieDetailsScreen extends React.Component {
-  static navigationOptions = ({navigation}) => ({
+  static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.item.name,
-    headerTitleStyle:{color:'#fff'},
+    headerTitleStyle: { color: '#fff' },
     headerStyle: { backgroundColor: '#512DA8' },
+    headerLeft: 
+      <TouchableHighlight style={{ width: 35, height: 35, marginLeft: 10, alignItems: 'center', justifyContent: 'center'}}
+        activeOpacity={0.8}
+        underlayColor='#512DA8'
+        onPress={() => navigation.goBack(null)} >
+          <Image source={require('./images/navigation_back_white.png')} style={{ width: 25, height: 25 }} />
+      </TouchableHighlight>
+    ,
   });
 
   render() {
@@ -15,10 +23,37 @@ export default class MovieDetailsScreen extends React.Component {
     let imgHeight = imgWidth * movieDetatil.imageOfMovieHeight / movieDetatil.imageOfMovieWidth;
     return (
       <View>
-        <Text>{movieDetatil.summaryOfMovie}</Text>
-        <Image source={{uri:movieDetatil.printscreen}} style={{width:imgWidth,height:imgHeight}}/>
-        <Text>{movieDetatil.yunPassword}</Text>
+        <ScrollView >
+          <Text style={styles.textStyle}>{movieDetatil.summaryOfMovie}</Text>
+          <Image source={{ uri: movieDetatil.printscreen }} style={{
+            width: imgWidth, height: imgHeight,
+            marginLeft: 10, marginRight: 10,
+          }} />
+          <Text style={styles.textStyle}>{movieDetatil.yunPassword}</Text>
+
+        </ScrollView >
+        <TouchableHighlight style={[styles.imgButtonStyle, { position: 'absolute', bottom: 20, right: 20 }]} onPress={
+          () => { Linking.openURL(movieDetatil.baiduyun).catch(err => console.error('An error occurred', err)); }
+        }>
+          <Image source={require('./images/link.png')} style={{ width: 30, height: 30 }} />
+        </TouchableHighlight>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textStyle: {
+    color: '#212121',
+    margin: 10
+  },
+  imgButtonStyle: {
+    width: 50, height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#512DA8',
+    borderColor: '#512DA8',
+    borderRadius: 25,
+    borderWidth: (Platform.OS === 'ios' ? 1.0 : 1.5) / PixelRatio.get(),
+  }
+});
